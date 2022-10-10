f6357b9e3cc6b8bd3bf0824f35eb6e8947f74772
Debin <devinaliano99@gmail.com>
2022-10-11 00:41:55 +0800

    rewrite brightness function + code optimization

--- src/Main.py
+++ src/Main.py
@@ -1,11 +1,13 @@
 from tkinter import *
 from tkinter import filedialog
+from tkinter import simpledialog
 from PIL import ImageTk, Image
+import time as t
 
-def butopenfile():
+def openfile():
     
     global openinput
-    window.filename = filedialog.askopenfilename(initialdir="./", filetypes=(("Image Files", "*.png *.jpg *.jpeg"), ("All Files", "*.*")))
+    window.filename = filedialog.askopenfilename(initialdir="../test", filetypes=(("Image Files", "*.png *.jpg *.jpeg"), ("All Files", "*.*")))
     alamatfile = window.filename
     
     if alamatfile:
@@ -13,8 +15,8 @@ def butopenfile():
         dictfotoinput["image"] = ImageTk.PhotoImage(openinput)
         labelfotoinput.configure(image=dictfotoinput["image"])
 
-def butgrayscale():
-
+def convert_grayscale():
+    imagedata.clear()
     datafotograyscale = newfotograyscale.load()
     
     for xpos in range(openinput.width):
@@ -23,39 +25,32 @@ def butgrayscale():
             gbuf = openinput.getpixel((xpos,ypos))[1]
             bbuf = openinput.getpixel((xpos,ypos))[2]
             imagedata.append([xpos, ypos, rbuf, gbuf, bbuf])
-
-    for data in imagedata:
+ 
+    #mengubah rgb value ke grayscale & update array
+    for i, data in enumerate(imagedata):
         x,y,r,g,b = data
         grayscale = ((r+g+b)/3)
         grayscale = int(grayscale)
-        datafotograyscale[x,y] = (grayscale,grayscale,grayscale)
+        imagedata[i] = x,y,grayscale
         
+        datafotograyscale[x,y] = (grayscale,grayscale,grayscale)
+
     dictfotograyscale["image"] = ImageTk.PhotoImage(newfotograyscale)
     labelfotograyscale.configure(image=dictfotograyscale["image"])
-    labelfotograyscale.image = dictfotograyscale["image"]
 
-def butbrightness():
-
-    windowbrightness = Toplevel()
+def adjust_brightness():
     datafotobrightness = newfotohasil.load()
+    value = simpledialog.askinteger(title= "Brightness Value", prompt= "Value") 
     
-    def getvalue():
-         
-        bright = int(entrybrightness.get())
-        
+    if value:
         for data in imagedata:
-            x,y,r,g,b = data
-            grayscale = ((r+g+b)/3)
-            grayscale = int(grayscale)
-            brightadjust = (grayscale + bright)
-            datafotobrightness[x,y] = (brightadjust,brightadjust,brightadjust)
+            x,y,grayscale = data
+            newgray = grayscale + value
             
-        windowbrightness.destroy()
+            datafotobrightness[x,y] = (newgray, newgray, newgray)
         
-    entrybrightness = Entry(windowbrightness)
-    okbrightbutton = Button(windowbrightness, text="Enter", command=getvalue)
-    entrybrightness.grid(row=1, column=1)
-    okbrightbutton.grid(row=1, column=2)
+    dictfotohasil["image"] = ImageTk.PhotoImage(newfotohasil)
+    labelfotohasil.configure(image=dictfotohasil["image"])
     
 def restart():
     
@@ -71,13 +66,11 @@ def restart():
     dictfotohasil['image'] = ImageTk.PhotoImage(newfotohasil)
     labelfotohasil.configure(image = dictfotohasil["image"])
 
-def printtext():
-    print(1)
-
 """  Start Tkinter Window  """
 window = Tk()
 window.geometry('+500+190') #window position
 window.resizable(0,0)
+window.title("Pointwise Grayscale")
 
 """  Deklarasi Variabel  """
 ukuran = (300,450)
@@ -107,10 +100,10 @@ labelfotograyscale.pack()
 labelfotohasil.pack()
 
 """  Button  """
-buttoninput = Button(window, text="Open Files", command=butopenfile)
-buttongrayscale = Button(window, text="Grayscale", command=butgrayscale)
-buttonbrightness = Button(window, text="Brightness Adjusment", command=butbrightness)
-buttonnegation = Button(window, text="Negation", command=printtext)
+buttoninput = Button(window, text="Open Files", command=openfile)
+buttongrayscale = Button(window, text="Grayscale", command=convert_grayscale)
+buttonbrightness = Button(window, text="Brightness Adjusment", command=adjust_brightness)
+buttonnegation = Button(window, text="Negation")
 buttonrestart = Button(window, text="Reset", command=restart)
 
 framefotoinput.grid(row=1, column=1, sticky=EW)
